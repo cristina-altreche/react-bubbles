fbbe9e612b3897e2663e73a1197571ddff66961b
Cristina Altreche <c.altreche@gmail.com>
2020-07-24 14:02:04 -0400

    Questions answered

--- client/src/utils/axiosWithAuth.js
+++ client/src/utils/axiosWithAuth.js
@@ -1,9 +1,12 @@
 
 import axios from "axios";
 
+//this returns the token from the localStorage i.e. server
 export function getToken() {
   return localStorage.getItem("token");
 }
+
+//this creates the authorization for this url
 export default function() {
   return axios.create({
     baseURL: "http://localhost:5000",
